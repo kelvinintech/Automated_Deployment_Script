f71e047ec006a39c2dbd20b15b9d44f8e8318502
Kelvin R. Tobias <Kelvinrtobias@gmail.com>
2024-10-27 15:16:13 -0400

    Initial Commit

---
+++ deploy_script.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+def deploy_app():
+    print("Starting deployment..")
+    os.chdir(r'C:\Users\kelvi\OneDrive\Desktop\projects\Automated_Deployment_Script')
+    subprocess.run(['git', 'pull'])
+    subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+    subprocess.run(['systemctl', 'restart', 'myapp'])
+    print("Deployment is completed successfully!")
+
+
+
+
+
+if __name__ == "__main__":
+    deploy_app()
